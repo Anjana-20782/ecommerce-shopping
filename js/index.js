998e600d855563208ca93a270e4c097c3402112e
Anjana-20782 <anjanasivadasan0305@gmail.com>
2025-11-12 14:51:48 +0530

    new contents added

--- js/index.js
+++ js/index.js
@@ -1,27 +1,24 @@
-const data=``;
-async function fetchProducts()
-{
- try{
-      const res=await fetch(`https://dummyjson.com/products`)
-   const data=await res.json()
-    console.log(data);
-    
-    let str=""
-   data.products.forEach(element => {
-   
-    str+=`
-    <div class="card">
-        <img src="${element.thumbnail}">
-        <h3>${element.title}</h3>
-        <h5>${element.rating}</h5>
-        <p class="price">Price :${element.price}</p>
-    </div>
-    `;
-   });
-   document.getElementById("cards").innerHTML=str;
+async function fetchProducts() {
+  try {
+    const res = await fetch(`https://dummyjson.com/products`);
+    const data = await res.json();
+
+    let str = "";
+    data.products.forEach(element => {
+      str += `
+        <div class="card">
+          <a href="../pages/add.html?id=${element.id}">
+            <img src="${element.thumbnail}" alt="${element.title}">
+            <h3>${element.title}</h3>
+            <p>${element.rating}⭐️</p>
+            <p class="price">Price: $${element.price}</p>
+          </a>
+        </div>
+      `;
+    });
+    document.getElementById("cards").innerHTML = str;
+  } catch (error) {
+    console.log("Error fetching data:", error);
+  }
 }
-catch(error){
-    console.log("Error fetching data:",error);
-}
-}
-fetchProducts()
+fetchProducts();
